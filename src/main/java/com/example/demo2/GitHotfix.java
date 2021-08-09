62973a27b78e0d595076a36a68bf2076dedde8a5
kaki99 <99kaki@inswave.com>
2021-08-09 14:44:46 +0900

    eeee

---
+++ src/main/java/com/example/demo2/GitHotfix.java
@@ -0,0 +1,10 @@
+package com.example.demo2;
+
+public class GitHotfix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hotfix merge");
+	}
+
+}
