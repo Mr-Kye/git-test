0e30707f9873db1aba0823cd00e53c31a05d38a1
DESKTOP-3IR8CMU\User <99kaki@inswave.com>
2021-08-09 13:49:05 +0900

    add66

---
+++ src/main/java/com/example/demo1/GitDemo.java
@@ -0,0 +1,14 @@
+package com.example.demo1;
+
+public class GitDemo {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
